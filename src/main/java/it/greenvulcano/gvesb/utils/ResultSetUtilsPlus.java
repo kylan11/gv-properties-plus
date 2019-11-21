d9fb38dd45c02305407182b85f82bfb2f346d38e
Francesco Lauritano <50871360+kylan11@users.noreply.github.com>
2019-11-22 00:42:36 +0100

    Update ResultSetUtilsPlus.java

--- src/main/java/it/greenvulcano/gvesb/utils/ResultSetUtilsPlus.java
+++ src/main/java/it/greenvulcano/gvesb/utils/ResultSetUtilsPlus.java
@@ -3,72 +3,28 @@ package it.greenvulcano.gvesb.utils;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 public class ResultSetUtilsPlus {
-	
-	  public static String getResultSetAsJSON( ResultSet rs )
-			    throws SQLException, JSONException
-			  {
-			    JSONArray json = new JSONArray();
-			    ResultSetMetaData rsmd = rs.getMetaData();
 
-			    while(rs.next()) {
-			      int numColumns = rsmd.getColumnCount();
-			      JSONObject obj = new JSONObject();
+	public static String getResultSetAsJSON(ResultSet rs) throws SQLException {
+		{
+			JSONArray json = new JSONArray();
+			ResultSetMetaData metadata = rs.getMetaData();
+			int numColumns = metadata.getColumnCount();
 
-			      for (int i=1; i<numColumns+1; i++) {
-			        String column_name = rsmd.getColumnName(i);
-
-			        if(rsmd.getColumnType(i)==java.sql.Types.ARRAY){
-			         obj.put(column_name, rs.getArray(column_name));
-			        }
-			        else if(rsmd.getColumnType(i)==java.sql.Types.BIGINT){
-			         obj.put(column_name, rs.getInt(column_name));
-			        }
-			        else if(rsmd.getColumnType(i)==java.sql.Types.BOOLEAN){
-			         obj.put(column_name, rs.getBoolean(column_name));
-			        }
-			        else if(rsmd.getColumnType(i)==java.sql.Types.BLOB){
-			         obj.put(column_name, rs.getBlob(column_name));
-			        }
-			        else if(rsmd.getColumnType(i)==java.sql.Types.DOUBLE){
-			         obj.put(column_name, rs.getDouble(column_name)); 
-			        }
-			        else if(rsmd.getColumnType(i)==java.sql.Types.FLOAT){
-			         obj.put(column_name, rs.getFloat(column_name));
-			        }
-			        else if(rsmd.getColumnType(i)==java.sql.Types.INTEGER){
-			         obj.put(column_name, rs.getInt(column_name));
-			        }
-			        else if(rsmd.getColumnType(i)==java.sql.Types.NVARCHAR){
-			         obj.put(column_name, rs.getNString(column_name));
-			        }
-			        else if(rsmd.getColumnType(i)==java.sql.Types.VARCHAR){
-			         obj.put(column_name, rs.getString(column_name));
-			        }
-			        else if(rsmd.getColumnType(i)==java.sql.Types.TINYINT){
-			         obj.put(column_name, rs.getInt(column_name));
-			        }
-			        else if(rsmd.getColumnType(i)==java.sql.Types.SMALLINT){
-			         obj.put(column_name, rs.getInt(column_name));
-			        }
-			        else if(rsmd.getColumnType(i)==java.sql.Types.DATE){
-			         obj.put(column_name, rs.getDate(column_name));
-			        }
-			        else if(rsmd.getColumnType(i)==java.sql.Types.TIMESTAMP){
-			        obj.put(column_name, rs.getTimestamp(column_name));   
-			        }
-			        else{
-			         obj.put(column_name, rs.getObject(column_name));
-			        }
-			      }
-			      json.put(obj);
-			    }
-			    
-			    return json.length() == 1 ? json.get(0).toString() : json.toString();
-			  }
+			while (rs.next()) // iterate rows
+			{
+				JSONObject obj = new JSONObject(); // extends HashMap
+				for (int i = 1; i <= numColumns; ++i) // iterate columns
+				{
+					String column_name = metadata.getColumnName(i);
+					obj.put(column_name, rs.getObject(column_name));
+				}
+				json.put(obj);
+			}
+			return json.length() == 1 ? json.get(0).toString() : json.toString();
+		}
+	}
 }
